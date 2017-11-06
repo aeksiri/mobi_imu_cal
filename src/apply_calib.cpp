129b863c0007c56dc6bcc0e575b8e158054d308d
Akkharaphong EKSIRI <akkharaphong.e@bu.ac.th>
2017-11-06 10:02:01 +0700

    Update apply_calib.cpp

--- src/apply_calib.cpp
+++ src/apply_calib.cpp
@@ -73,7 +73,7 @@ ApplyCalib::ApplyCalib() :
   mag_pub_ = nh.advertise<sensor_msgs::MagneticField>("imu/mag", queue_size);
 }
 
-void ApplyCalib::rawImuCallback(lino_msgs::Imu::ConstPtr raw)
+void ApplyCalib::rawImuCallback(mobi_msgs::Imu::ConstPtr raw)
 {
   if (calibrate_gyros_)
   {
